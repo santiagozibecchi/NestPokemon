52174690daa812f00d2516b13800d260908450b2
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2025-04-20 19:32:11 -0300

    init mongo conection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // Coneccion a MongoDB
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule
   ],
 })
