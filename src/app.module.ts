83e46da583fb5e5bfefba23fbf6ddf73a4afbdc9
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2025-04-20 18:59:49 -0300

    create pokemon module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-    })
+    }),
+    PokemonModule
   ],
 })
 export class AppModule {}
