04fcf416fe342e5a86de2f4760c151856a347907
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2025-04-21 20:19:47 -0300

    chore: add imports module notes

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -7,11 +7,21 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  /**
+   * La clave imports permite componer módulos, es decir, usar funcionalidades que han sido definidas en otros módulos.
+   * NestJS es altamente modular, y los módulos pueden depender unos de otros.
+   */
   imports: [
+    /**
+     * Para registrar un modelo de Mongoose
+     * Esto es útil cuando deseas encapsular la lógica de una entidad
+     * NestJS ahora sabe cómo interactuar con la colección pokemons de MongoDB desde este módulo.
+     * Esto permite que puedas inyectar el modelo Pokemon en tu servicio (PokemonService):
+     */
     MongooseModule.forFeature([
       {
-        name: Pokemon.name,
-        schema: PokemonSchema,
+        name: Pokemon.name, // nombre del modelo.
+        schema: PokemonSchema, // clase decorada como entidad (@Schema())
       }
     ])
   ],
