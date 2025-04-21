fc1988757dd5da67dd48a04d67603cde1da8c929
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2025-04-21 20:20:29 -0300

    chore: create and find one pokemon

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsString, IsNumber, MinLength, IsPositive, Min } from 'class-validator'
+
+export class CreatePokemonDto {
+
+  @IsString()
+  @MinLength(1)
+  name: string
+
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  no: number
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,22 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { isValidObjectId, Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) { }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon exist in DB ${JSON.stringify(error.keyValue)}`)
+      }
+      throw new InternalServerErrorException('Cant create Pokemon, check server logs')
+    }
+
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    const normalizedTerm = term.toLowerCase().trim();
+    const pokemon = await this.tryFindStrategies(normalizedTerm);
+    if (!pokemon) {
+      throw new NotFoundException(`Pokemon with id|name|no '${term}' not found`);
+    }
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+  private async tryFindStrategies(term: string): Promise<Pokemon | null> {
+    const strategies = [
+      async () => !isNaN(+term) ? await this.pokemonModel.findOne({ no: term }) : null,
+      async () => isValidObjectId(term) ? await this.pokemonModel.findById(term) : null,
+      async () => await this.pokemonModel.findOne({ name: term })
+    ];
+    for (const strategy of strategies) {
+      const result = await strategy(); // patrón "short-circuit" 
+      if (result) return result;
+    }
+    return null;
+  }
+
+  async update(id: number, updatePokemonDto: UpdatePokemonDto) {
     return `This action updates a #${id} pokemon`;
   }
 
